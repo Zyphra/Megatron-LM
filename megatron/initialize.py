1e3f732c6328ae1e9bbefbc65a2111dd461cdd78
pglorio <85982602+pglorio@users.noreply.github.com>
2023-11-17 18:49:17 +0100

    Update initialize.py

--- megatron/initialize.py
+++ megatron/initialize.py
@@ -91,6 +91,10 @@ def initialize_megatron(
         if args.tp_comm_overlap:
            _initialize_tp_communicators()
 
+    dir_path = os.path.join(args.router_profiling_path)
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
         # No continuation function
         return None
 
