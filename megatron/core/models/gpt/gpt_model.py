b6c79070e415d8bb124d52e8fea014cdf9f7adcc
pglorio <85982602+pglorio@users.noreply.github.com>
2023-11-21 02:08:42 +0100

    Update gpt_model.py

--- megatron/core/models/gpt/gpt_model.py
+++ megatron/core/models/gpt/gpt_model.py
@@ -167,7 +167,7 @@ class GPTModel(LanguageModule):
         print('LOSS:', loss.sum())
         if hasattr(self.decoder, 'l_aux_tot'):
             denom = loss.numel()
-            loss +=  1000 * self.decoder.l_aux_tot / denom
+            loss +=  000 * self.decoder.l_aux_tot / denom
             print('TOTAL L_AUX:', self.decoder.l_aux_tot, 'NEW TOTAL LOSS:', loss.sum())
 
         return loss
