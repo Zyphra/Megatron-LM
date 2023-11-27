d52cff0ac1ee4995c62fc1c16c72fa33193e3661
BerenMillidge <Bmillidgework@gmail.com>
2023-11-27 01:46:00 +0000

    updates to generation

--- generation_test.py
+++ generation_test.py
@@ -19,11 +19,14 @@ import torch
 
 # To run on multiGPU node use `torchrun --nproc_per_node 8 --nnodes 1 --node_rank 0 --master_addr localhost --master_port 6000 generation_test.py`
 
+#/opt/Megatron-LM/examples/megarun_slurm/moe_1p3B_8E_bare.sh --checkpoint /checkpoints/megarun/ckpts_1p3B_bf16 
+
 os.environ["CUDA_DEVICE_MAX_CONNECTIONS"] = "1"
 os.environ["MASTER_ADDR"] = "localhost"
 os.environ["MASTER_PORT"] = "6000"
 
-config_path = "examples/moe_1p3B_8E_bare_r0.sh"
+config_path = "/opt/Megatron-LM/examples/megarun_slurm/moe_1p3B_8E_bare.sh"
+#config_path = "/opt/Megatron-LM/examples/moe_1p3B_8E_bare_r0.sh"
 
 with open(config_path,"r") as f:
     filestr = f.read()
@@ -41,13 +44,18 @@ def extract_keyword_args(filestr, keyword):
     return gpt_args.strip().split(" ")
 print(extract_keyword_args(filestr, "GPT_ARGS"))
 
+#VOCAB_FILE=/datasets/SlimPajama-627B_megatron/gpt-neox-20b-tokenizer/vocab.json
+#MERGE_FILE=/datasets/SlimPajama-627B_megatron/gpt-neox-20b-tokenizer/merges.txt
+#DATA_PATH=/datasets/SlimPajama-627B_megatron/gpt-neox-20b-tokenizer/train_text_document
+
 def extract_data_paths(filestr):
-    #vocab_file = filestr.split("VOCAB_FILE=")[1].split("\n")[0]
-    #merge_file = filestr.split("MERGE_FILE=")[1].split("\n")[0]
-    vocab_file = "/datasets/SlimPajama-627B_megatron/gpt-neox-20b-tokenizer/vocab.json"
-    merge_file = "/datasets/SlimPajama-627B_megatron/gpt-neox-20b-tokenizer/merges.txt"
+    vocab_file = filestr.split("VOCAB_FILE=")[1].split("\n")[0]
+    merge_file = filestr.split("MERGE_FILE=")[1].split("\n")[0]
+    #vocab_file = "/datasets/SlimPajama-627B_megatron/gpt-neox-20b-tokenizer/vocab.json"
+    #merge_file = "/datasets/SlimPajama-627B_megatron/gpt-neox-20b-tokenizer/merges.txt"
     #checkpoint_path = "/workspace/ckpts_bf16_125m"
-    checkpoint_path = "/checkpoints/megarun/ckpts_1p3b"
+    checkpoint_path = "/checkpoints/megarun/ckpts_1p3b_bf16"
+    #checkpoint_path = "/checkpoints/megarun/ckpts_1p3B"
     
     data_path = filestr.split("DATA_PATH=")[1].split("\n")[0]
     return ["--data-path", data_path, "--vocab-file" , vocab_file, "--merge-file" , merge_file,"--load" , checkpoint_path]
@@ -55,7 +63,7 @@ def extract_data_paths(filestr):
 print(extract_data_paths(filestr))
 
 
-sys.argv = ["checkpointing_tests.py"] # a hack to get around the jupyter issues in the sys.argc which we are messing with
+sys.argv = ["generation_test.py"] # a hack to get around the jupyter issues in the sys.argc which we are messing with
 sys.argv += extract_keyword_args(filestr, "GPT_ARGS")
 sys.argv += extract_data_paths(filestr)
 print(sys.argv)
@@ -64,6 +72,7 @@ print(sys.argv)
 def model_provider(pre_process=True, post_process=True):
     """Build the model."""
     args = get_args()
+    print("IN MODEL PRIVIDER ARGHS: ", args)
     args.recompute_granularity = None # enforce for inference
     config = core_transformer_config_from_args(args)
 
@@ -80,10 +89,14 @@ def add_text_generate_args(parser):
 
 initialize_megatron(extra_args_provider=add_text_generate_args,
                     args_defaults={'tokenizer_type': 'HFAutoTokenizer',
-                                    '--hf_autotokenizer_model': 'EleutherAI:gpt-neox-20b'
+                                    'hf_autotokenizer_model': 'EleutherAI/gpt-neox-20b',
                                     'no_load_rng': True,
                                     'no_load_optim': True})
 
+
+# 'tokenizer_type': 'GPT2BPETokenizer'
+#'tokenizer_type': 'HFAutoTokenizer',
+#                                    'hf_autotokenizer_model': 'EleutherAI/gpt-neox-20b',
 args = get_args()
 print("ARGS: ", args)
 
@@ -94,7 +107,7 @@ model = model[0]
 
 # %%
 #print("MODEL: ", model)
-prompts = ["How is your day?"]
+prompts = ["Testing testing 123"]
 tokens_to_generate = 100
 logprobs = True
 top_k = 0.0
