70a46ac9b864dc61a482afceae00c4d1da0d2064
pglorio <85982602+pglorio@users.noreply.github.com>
2023-11-28 19:18:01 -0800

    Update initialize.py

--- megatron/initialize.py
+++ megatron/initialize.py
@@ -90,10 +90,11 @@ def initialize_megatron(
 
         if args.tp_comm_overlap:
            _initialize_tp_communicators()
-
-    dir_path = os.path.join(args.router_profiling_path)
-    if not os.path.exists(dir_path) and torch.distributed.get_rank() == 0 and dir_path is not None:
-        os.makedirs(dir_path)
+            
+    if args.router_profiling_path is not None and torch.distributed.get_rank() == 0:
+        dir_path = os.path.join(args.router_profiling_path)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
 
         # No continuation function
         return None
