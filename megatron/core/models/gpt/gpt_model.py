163d697d5298002c02ceb2f84f4b45fd0e131657
pglorio <85982602+pglorio@users.noreply.github.com>
2023-11-29 23:35:32 -0800

    Update gpt_model.py

--- megatron/core/models/gpt/gpt_model.py
+++ megatron/core/models/gpt/gpt_model.py
@@ -168,15 +168,15 @@ class GPTModel(LanguageModule):
             # [s b h] => [b s h]
             return logits.transpose(0, 1).contiguous()
 
-        if args.curr_iteration % 2 == 0:
-            loss = self.compute_language_model_loss(labels, logits)
-        else:
-            if args.use_balancing_loss:
-                loss = args.l_aux
-            else:
-                loss = self.compute_language_model_loss(labels, logits)
-        # if args.use_balancing_loss:
-        #    loss += 0.1*args.l_aux
+        # if args.curr_iteration % 2 == 0:
+        loss = self.compute_language_model_loss(labels, logits)
+        #else:
+        #    if args.use_balancing_loss:
+        #        loss = args.l_aux
+        #    else:
+        #        loss = self.compute_language_model_loss(labels, logits)
+        if args.use_balancing_loss:
+           loss += args.l_aux
 
         return loss
 
