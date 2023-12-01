e38ca6542a40432c7fefb7c56299421d1b744ee2
Quentin Anthony <qganthony@yahoo.com>
2023-12-01 23:01:26 +0000

    update 32E script

--- examples/megarun_slurm/equisizing_suite/moe_32E_bare.sh
+++ examples/megarun_slurm/equisizing_suite/moe_32E_bare.sh
@@ -53,15 +53,15 @@ NNODES=2
 NODE_RANK=$current_rank
 WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
 
-CHECKPOINT_PATH=/checkpoints/megarun/ckpts_1p3b_bf16
+CHECKPOINT_PATH=/checkpoints/equiparam/32e
 VOCAB_FILE=/datasets/SlimPajama-627B_megatron/gpt-neox-20b-tokenizer/vocab.json
 MERGE_FILE=/datasets/SlimPajama-627B_megatron/gpt-neox-20b-tokenizer/merges.txt
 DATA_PATH=/datasets/SlimPajama-627B_megatron/gpt-neox-20b-tokenizer/train_text_document
 EXPERT_STATS_PATH=/checkpoints/equiparameter/32e
 
-WANDB_PROJECT=moe
-WANDB_EXP_NAME=final_moe_1p3b_8e_600B_slimpj_bf16
-WANDB_SAVE_DIR=/checkpoints/megarun/wandb_bf16
+WANDB_PROJECT=moe_equiparam
+WANDB_EXP_NAME=32e
+WANDB_SAVE_DIR=/checkpoints/equiparam/wandb_32e
 
 TOKENIZER_TYPE=HFAutoTokenizer
 TOKENIZER_MODEL="EleutherAI/gpt-neox-20b"
@@ -75,24 +75,24 @@ DISTRIBUTED_ARGS="
 "
 
 GPT_ARGS="
-    --num-layers 24 \
-    --hidden-size 2048 \
+    --num-layers 10 \
+    --hidden-size 704 \
     --num-attention-heads 16 \
     --seq-length 2048 \
     --max-position-embeddings 2048 \
-    --micro-batch-size 6 \
-    --global-batch-size 1152 \
-    --lr 0.00025 \
+    --micro-batch-size 30 \
+    --global-batch-size 480 \
+    --lr 0.00055 \
     --override-opt_param-scheduler \
-    --train-iters 290000 \
-    --lr-decay-iters 290000 \
+    --train-iters 580000 \
+    --lr-decay-iters 580000 \
     --lr-decay-style cosine \
-    --min-lr 1.0e-6 \
+    --min-lr 1.5e-6 \
     --weight-decay 0.0 \
     --lr-warmup-fraction .01 \
     --clip-grad 1.0 \
     --bf16 \
-    --num-experts 8 \
+    --num-experts 32 \
     --expert-model-parallel-size 8 \
     --recompute-granularity selective \
     --use-flash-attn \
@@ -127,10 +127,11 @@ OUTPUT_ARGS="
     --wandb-project $WANDB_PROJECT \
     --wandb-exp-name $WANDB_EXP_NAME \
     --wandb-save-dir $WANDB_SAVE_DIR \
-    --router-profiling-path $EXPERT_STATS_PATH
+    --router-profiling-path $EXPERT_STATS_PATH \
 "
 
 #pip list
+
 
 torchrun $DISTRIBUTED_ARGS /opt/Megatron-LM/pretrain_gpt.py \
     $GPT_ARGS \
