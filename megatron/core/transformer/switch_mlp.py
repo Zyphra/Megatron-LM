a135f0872d7a1686d39aeed2f5678adcd4eaec2e
pglorio <85982602+pglorio@users.noreply.github.com>
2023-12-03 00:44:23 -0800

    Update switch_mlp.py

--- megatron/core/transformer/switch_mlp.py
+++ megatron/core/transformer/switch_mlp.py
@@ -38,10 +38,10 @@ def save_token_count(token_count, layer, iteration, router_profiling_path):
     with open(os.path.join(router_profiling_path, 'token_counts.pkl'), 'ab') as file:
         pickle.dump([iteration, layer, token_count_list], file)
 
-def save_global_indices(global_indices, layer, iteration, router_profiling_path):
-    idx = global_indices.cpu().tolist()
+def save_global_indices(idx, layer, iteration, router_profiling_path):
     with open(os.path.join(router_profiling_path, 'global_indices.pkl'), 'ab') as file:
         pickle.dump([iteration, layer, idx], file)
+        print('SAVED THIS: ', [iteration, layer, idx])
 
 class SwitchMLP(MegatronModule):
     """
@@ -178,7 +178,8 @@ class SwitchMLP(MegatronModule):
                                            torch.bincount(global_indices_2, minlength=args.num_experts)])
             save_token_count(token_count, self.layer, args.curr_iteration, args.router_profiling_path)
             if args.curr_iteration % 100 == 0:
-                save_global_indices(global_indices, self.layer, args.curr_iteration, args.router_profiling_path)
+                idx = global_indices.cpu().tolist()
+                save_global_indices(idx, self.layer, args.curr_iteration, args.router_profiling_path)
 
         output_total = torch.zeros_like(global_hidden_states)
         if self.routing == 'top2' or self.routing == 'sinkhorn_top2':
