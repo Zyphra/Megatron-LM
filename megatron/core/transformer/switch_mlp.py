0cc6d573600f054769eb7f60edec7260888effe9
pglorio <85982602+pglorio@users.noreply.github.com>
2024-01-01 21:59:17 -0600

    Update switch_mlp.py

--- megatron/core/transformer/switch_mlp.py
+++ megatron/core/transformer/switch_mlp.py
@@ -27,15 +27,12 @@ def sinkhorn(cost, tol=0.0001):
     eps = 0.00000001
     error = 1e9
     d1_old = d1
-    t = 0
     while error > tol:
         t += 1
         d0 = (1 / d0.size(0)) * 1 / (torch.sum(d1 * cost, 1) + eps)
         d1 = (1 / d1.size(0)) * 1 / (torch.sum(d0.unsqueeze(1) * cost, 0) + eps)
         error = torch.mean(torch.abs(d1_old - d1))
         d1_old = d1
-    if t > 1:
-        print('NUMBER OF STEPS:', t)
     return d1 * cost * d0.unsqueeze(1)
 
 def save_token_count(token_count, layer, iteration, router_profiling_path):
